772d60d7bfbd3eb810d001faaa5fa1bafebc446f
abishekparajuli-np <abishekparajuli.np@gmail.com>
2026-01-07 23:38:50 +0545

    nn

--- main/helper.py
+++ main/helper.py
@@ -1,16 +1,16 @@
+from urlextract import URLExtract
+extractor=URLExtract()
+
+
 def fetch_stats(selected_user,df):
-    if selected_user == 'Overall':
-        num_messages=df.shape[0]
-        n_words=num_words(df)
+    if selected_user != 'Overall':
+        df=df[df['Users']==selected_user]
+    num_messages=df.shape[0]
+    n_words=num_words(df)
+    n_medias=num_medias(df)
+    n_links=num_links(df)
 
-        return num_messages,n_words
-    
-    else:
-        new_df=df[df['Users']==selected_user]
-        num_messages=new_df.shape[0]
-        n_words=num_words(new_df)
-
-        return num_messages,n_words
+    return num_messages,n_words,n_medias,n_links
 
 
 
@@ -21,3 +21,12 @@ def num_words(df):
         words.extend(message.split())
     return len(words)
 
+def num_medias(df):
+    n_medias=df[df['Message'] == '<Media omitted>\n'].shape[0]
+    return n_medias
+
+def num_links(df):
+    links=[]
+    for message in df['Message']:
+        links.extend(extractor.find_urls(message))
+    return len(links)
--- main/routes.py
+++ main/routes.py
@@ -1,31 +1,29 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, jsonify
 from main.preprocessor import preprocess
 import os
 import pandas as pd
 from main import app
 from main.helper import fetch_stats
 
-
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    chat_files = None
     user_list = []
-    num_messages = num_words = None
+    num_messages = num_words = num_medias=num_links= None
+    results = None
 
     if request.method == 'POST':
-        chat_files = request.files.get('chat_files')
+        chat_file = request.files.get('chat_file')
 
-        if chat_files and chat_files.filename != "":
+        if chat_file and chat_file.filename != "":
             filepath = os.path.join(
                 app.config['UPLOAD_FOLDER'],
-                chat_files.filename
+                chat_file.filename
             )
-            chat_files.save(filepath)
+            chat_file.save(filepath)
 
             with open(filepath, 'r', encoding='utf-8') as f:
                 data = f.read()
@@ -39,13 +37,33 @@ def main():
             user_list.sort()
             user_list.insert(0, 'Overall')
 
-            selected_user = request.form.get('selected_user', 'Overall')
-            num_messages, num_words = fetch_stats(selected_user, df)
+            return jsonify({'user_list': user_list})
 
     return render_template(
         'whatsapp_chat_analysis.html',
         title='Whatsapp Chat Analysis',
-        user_list=user_list,
-        num_messages=num_messages,
-        num_words=num_words
+        user_list=[]
     )
+
+@app.route('/fetch_stats', methods=['POST'])
+def fetch():
+    selected_user = request.json.get('selected_user')
+    chat_file_name = request.json.get('file_name')
+
+    filepath = os.path.join(
+        app.config['UPLOAD_FOLDER'],
+        chat_file_name
+    )
+
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    df = preprocess(data)
+    num_messages, num_words, num_medias, num_links= fetch_stats(selected_user, df)
+    results = {
+        'num_messages': num_messages,
+        'num_words': num_words,
+        'num_medias':num_medias,
+        'num_links':num_links,
+    }
+    return jsonify({'results': results})
